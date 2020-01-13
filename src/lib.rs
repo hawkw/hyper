bed81bc64e952e088288d05f5919c181b3ecbee4
Eliza Weisman <eliza@buoyant.io>
2020-01-13 15:31:24 -0800

    test
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/lib.rs
+++ src/lib.rs
@@ -18,56 +18,60 @@
 //! [reqwest](https://crates.io/crates/reqwest) crate.
 
 extern crate bytes;
-#[macro_use] extern crate futures;
-#[cfg(feature = "runtime")] extern crate futures_cpupool;
+#[macro_use]
+extern crate futures;
+#[cfg(feature = "runtime")]
+extern crate futures_cpupool;
 extern crate h2;
-#[doc(hidden)] pub extern crate http;
+#[doc(hidden)]
+pub extern crate http;
 extern crate http_body;
 extern crate httparse;
 extern crate iovec;
 extern crate itoa;
-#[macro_use] extern crate log;
-#[cfg(feature = "runtime")] extern crate net2;
+#[macro_use]
+extern crate tracing;
+#[cfg(feature = "runtime")]
+extern crate net2;
 extern crate time;
-#[cfg(feature = "runtime")] extern crate tokio;
+#[cfg(feature = "runtime")]
+extern crate tokio;
 extern crate tokio_buf;
-#[cfg(feature = "runtime")] extern crate tokio_executor;
-#[macro_use] extern crate tokio_io;
-#[cfg(feature = "runtime")] extern crate tokio_reactor;
-#[cfg(feature = "runtime")] extern crate tokio_tcp;
-#[cfg(feature = "runtime")] extern crate tokio_threadpool;
-#[cfg(feature = "runtime")] extern crate tokio_timer;
+#[cfg(feature = "runtime")]
+extern crate tokio_executor;
+#[macro_use]
+extern crate tokio_io;
+#[cfg(feature = "runtime")]
+extern crate tokio_reactor;
+#[cfg(feature = "runtime")]
+extern crate tokio_tcp;
+#[cfg(feature = "runtime")]
+extern crate tokio_threadpool;
+#[cfg(feature = "runtime")]
+extern crate tokio_timer;
 extern crate want;
 
 #[cfg(all(test, feature = "nightly"))]
 extern crate test;
 
-pub use http::{
-    header,
-    HeaderMap,
-    Method,
-    Request,
-    Response,
-    StatusCode,
-    Uri,
-    Version,
-};
+pub use http::{header, HeaderMap, Method, Request, Response, StatusCode, Uri, Version};
 
-pub use client::Client;
-pub use error::{Result, Error};
 pub use body::{Body, Chunk};
+pub use client::Client;
+pub use error::{Error, Result};
 pub use server::Server;
 
 #[macro_use]
 mod common;
-#[cfg(test)]
-mod mock;
 pub mod body;
 pub mod client;
 pub mod error;
 mod headers;
+#[cfg(test)]
+mod mock;
 mod proto;
+#[cfg(feature = "runtime")]
+pub mod rt;
 pub mod server;
 pub mod service;
-#[cfg(feature = "runtime")] pub mod rt;
 pub mod upgrade;
